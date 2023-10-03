036d70809b31130859eba33340084be33fd800f2
anthuwan <36472537+anthuwan@users.noreply.github.com>
2023-10-04 00:20:46 +0530

    Update GlobalExceptionHandler.java

--- src/main/java/expense/calculator/app/exception/GlobalExceptionHandler.java
+++ src/main/java/expense/calculator/app/exception/GlobalExceptionHandler.java
@@ -1,18 +1,4 @@
-/*
- * InstaRem Overseas Money Transfer.
- *  https://www.instarem.com/en-in/
- *
- * Copyright (c) 2014-2020 InstaReM
- *
- * InstaRem is an acronym of Instant Remittance.
- * InstaRem Software is designed and developed to ease the Overseas Money Transfer.
- * It allows you to transfer your money overseas with minimum cost and time.
- *
- * This file is licensed and cannot be accessed by outside InstaRem.
- * It can only be accessed and modified by the authorized InstaRem Technical Teams.
- * If any unauthorized, outside of the InstaRem, user found to be unlawfully modified
- * the content of this file, will be prosecuted under the Copyright Act.
- */
+
 package expense.calculator.app.exception;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
